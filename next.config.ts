56dc80e5eeca7096dcf41ca3dcd95227928bc802
tracilvrd <oghenekparobo.ese@gmail.com>
2026-07-09 01:26:50 +0100

    feat: initialize Next.js project with TypeScript and Tailwind CSS
    - Added package.json with Next.js, React, and Tailwind CSS dependencies.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added SVG assets for icons and branding in the public directory.
    - Configured TypeScript with tsconfig.json for strict type checking and Next.js support.

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
+
+  allowedDevOrigins: [
+    "192.168.56.1",
+  ],
+};
+
+export default nextConfig;
