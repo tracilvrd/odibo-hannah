2f30f97ab976dd5d9bdbfa3fdab248545c87dcb3
tracilvrd <oghenekparobo.ese@gmail.com>
2026-07-14 09:56:18 +0100

    feat: add AnnouncementBar component for promotional messaging

---
+++ components/layout/announcement-bar.tsx
@@ -0,0 +1,18 @@
+import { Container } from "./container";
+
+export function AnnouncementBar() {
+  return (
+    <div className="bg-[#D62828] text-white">
+      <Container>
+        <div className="flex h-10 items-center justify-between text-xs md:text-sm">
+          <p>Premium Quality. Everyday Inspiration.</p>
+
+          <p>
+            Fast Delivery Across Nigeria |
+            Customer Care: +234 912 314 6458
+          </p>
+        </div>
+      </Container>
+    </div>
+  );
+}
